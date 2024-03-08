5d9a265cee0362da7bcac94fd8e0e423cc583eff
Nithin Joji0756 <nithinjoji0756@gmail.com>
2024-03-08 23:09:39 +0530

    create user form fronted form completed connected to backend through react query and fetch

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,20 +1,37 @@
 import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import * as apiClient from '../api/api-client'
 
-type RegisterFormData = {
+export type RegisterFormData = {
   firstName: string;
   lastName: string;
   email: string;
   password: string;
-  confirmPassword:string
+  confirmPassword: string;
 };
 
 const Register = () => {
-  const { register,watch,handleSubmit,formState:{errors}} = useForm<RegisterFormData>();
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>();
 
-  const onSubmit=handleSubmit((data)=>{
-    console.log(data);
-    
+  const mutation=useMutation(apiClient.register,{
+    onSuccess:()=>{
+      console.log("registration sucessfully");
+      
+    },
+    onError:(error:Error)=>{
+      console.log(error.message);
+      
+    }
   })
+
+  const onSubmit = handleSubmit((data) => {
+      mutation.mutate(data)
+  });
   return (
     <form className="flex flex-col gap-5" onSubmit={onSubmit}>
       <h2 className="text-3xl font-bold text-center">Create an Account</h2>
@@ -24,54 +41,80 @@ const Register = () => {
           <label htmlFor="" className="text-gray-700 text-sm font-bold flex-1">
             First Name
           </label>
-          <input type="text" className="border w-full py-1 px-2 font-normal" {...register("firstName",{required:"This field is required"})}></input>
-       <span className="text-red-500">{errors?.firstName?.message}</span>
+          <input
+            type="text"
+            className="border w-full py-1 px-2 font-normal"
+            {...register("firstName", { required: "This field is required" })}
+          ></input>
+          <span className="text-red-500">{errors?.firstName?.message}</span>
         </div>
 
         <div className=" w-full">
           <label htmlFor="" className="text-gray-700 text-sm font-bold flex-1">
             Last Name
           </label>
-          <input type="text" className="border w-full py-1 px-2 font-normal "  {...register("lastName",{required:"This field is required"})}></input>
+          <input
+            type="text"
+            className="border w-full py-1 px-2 font-normal "
+            {...register("lastName", { required: "This field is required" })}
+          ></input>
           <span className="text-red-500">{errors?.lastName?.message}</span>
         </div>
       </div>
       <label htmlFor="" className="text-gray-700 text-sm font-bold flex-1">
         Email
-          </label>
-          <input type="email" className="border w-full py-1 px-2 font-normal "  {...register("email",{required:"This field is required"})}></input>
-          <span className="text-red-500">{errors?.email?.message}</span>
-          {/* password */}
+      </label>
+      <input
+        type="email"
+        className="border w-full py-1 px-2 font-normal "
+        {...register("email", { required: "This field is required" })}
+      ></input>
+      <span className="text-red-500">{errors?.email?.message}</span>
+      {/* password */}
 
-          <label htmlFor="" className="text-gray-700 text-sm font-bold flex-1">
-     Password
-          </label>
-          <input type="password" className="border w-full py-1 px-2 font-normal "  {...register("password",{required:"This field is required",minLength:{
-             value:5,
-             message:"password must be atleast 5 characters"
-          }})}></input>
-  <span className="text-red-500">{errors?.password?.message}</span>
-          {/* confirm password  */}
+      <label htmlFor="" className="text-gray-700 text-sm font-bold flex-1">
+        Password
+      </label>
+      <input
+        type="password"
+        className="border w-full py-1 px-2 font-normal "
+        {...register("password", {
+          required: "This field is required",
+          minLength: {
+            value: 5,
+            message: "password must be atleast 5 characters",
+          },
+        })}
+      ></input>
+      <span className="text-red-500">{errors?.password?.message}</span>
+      {/* confirm password  */}
 
-          <label htmlFor="" className="text-gray-700 text-sm font-bold flex-1">
-         Confirm  Password
-          </label>
-          <input type="password" className="border w-full py-1 px-2 font-normal "  {...register("confirmPassword",{
-            validate:(val)=>{
-              if(!val){
-                return "this field is required"
-              }else if(watch('password') !== val){
-                return "your password do not match"
-              }
-              
-
+      <label htmlFor="" className="text-gray-700 text-sm font-bold flex-1">
+        Confirm Password
+      </label>
+      <input
+        type="password"
+        className="border w-full py-1 px-2 font-normal "
+        {...register("confirmPassword", {
+          validate: (val) => {
+            if (!val) {
+              return "this field is required";
+            } else if (watch("password") !== val) {
+              return "your password do not match";
             }
-          })}></input>
-            <span className="text-red-500">{errors?.confirmPassword?.message}</span>
+          },
+        })}
+      ></input>
+      <span className="text-red-500">{errors?.confirmPassword?.message}</span>
 
-<span>
-    <button type="submit" className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500">submit</button>
-</span>
+      <span>
+        <button
+          type="submit"
+          className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500"
+        >
+          submit
+        </button>
+      </span>
     </form>
   );
 };
